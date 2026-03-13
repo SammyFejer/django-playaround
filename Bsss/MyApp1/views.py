01da5bc052f3ffa1f50ce91d0e4a25949e8275c9
Sammy Fejer <0348257@schoolsnet.act.edu.au>
2026-03-13 11:49:01 +1100

    First broke commit

--- Bsss/MyApp1/views.py
+++ Bsss/MyApp1/views.py
@@ -1,7 +1,5 @@
 
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
 from .models import teacher 
 from .models import course_Area
 
@@ -9,12 +7,12 @@ from .models import course_Area
 # Create your views here.fffgg
 def index(request):
 
-   teach = teacher.objects.all(),
-   CA = course_Area.objects.all(),
+   teaching = teacher.objects.all(),
+   CAing = course_Area.objects.all(),
 
    stuff = {
-   'teach' : teach,
-   'CA' : CA,
+   'teach' : teaching,
+   'CA' : CAing,
    }
    return render(request, "MyApp1/index.html", {'content': stuff})
    
