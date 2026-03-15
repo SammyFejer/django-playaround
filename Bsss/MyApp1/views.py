5614fd519b011793e87ee37de61ed2576e7c2b7c
Sammy Fejer <0348257@schoolsnet.act.edu.au>
2026-03-16 10:28:11 +1100

    16/3

--- Bsss/MyApp1/views.py
+++ Bsss/MyApp1/views.py
@@ -7,15 +7,15 @@ from .models import course_Area
 # Create your views here.fffgg
 def index(request):
 
-   teaching = teacher.objects.all(),
-   CAing = course_Area.objects.all(),
+   teach = teacher.objects.all()
+   CA = course_Area.objects.all()
 
-   '''stuff = {
-   'teach' : teaching,
-   'CA' : CAing,
-   }'''
-   return render(request, "MyApp1/index.html", {'content': teaching, 'content2': CAing})
-   
+   #stuff = {
+   #'teach' : teaching,
+   #'CA' : CAing,
+   #}
+   return render(request, "MyApp1/index.html", {'content': teach, 'content2': CA})
+
 
   
 
